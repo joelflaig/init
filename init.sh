3e579813c484baed4bb6bef4ff6601f5554db9f8
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-04-20 00:39:41 +0200

    Modified to use homebrew.

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 # sudo apt update -y && sudo apt upgrade -y
-sudo pacman -Sy archlinux-keyring
 sudo pacman -Su
 
-sudo apt install git
-sudo apt install neovim
-sudo apt install eza
-sudo apt install bat
-sudo apt install stow
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
+brew install neovim
+brew install eza
+brew install bat
+brew install stow
