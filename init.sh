55646d1e65c9731d91b9bbe231a729e34ce5cde5
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-05-02 20:06:59 +0200

    Added sudo privileges.

--- init.sh
+++ init.sh
@@ -1,67 +1,63 @@
-# acquire admin privileges
-sudo su
-
 # update
-pacman -Su
+sudo pacman -Su
 
 # install homebrew -- my preferred package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-pacman -S base-devel
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo pacman -S base-devel
 
 # git -- version control (only if not installed)
 # sudo pacman -S git # pacman
 # brew install git # brew
 
 # gcc -- c compiler
-brew install gcc
+sudo brew install gcc
 
 # nim -- nim language
-brew install nim
+sudo brew install nim
 
 # tmux -- terminal multiplexer
-brew install tmux
-tmux
+sudo brew install tmux
 
 # neovim -- editor
-brew install neovim
+sudo brew install neovim
 
 # bat -- cat with syntax highlighting
-brew install bat
+sudo brew install bat
 
 # eza -- ls replacement
-brew install eza
+sudo brew install eza
 
 # lazygit -- git client
-brew install lazygit
+sudo brew install lazygit
 
 # delta -- diff tool
-brew install git-delta
+sudo brew install git-delta
 
 # fzf -- fuzzy finder
-brew install fzf
+sudo brew install fzf
 
 # ripgrep -- grep replacement
-brew install ripgrep
+sudo brew install ripgrep
 
 # stow -- symlink manager
-brew install stow
+sudo brew install stow
 
 # starship -- shell prompt
-brew install starship
+sudo brew install starship
 
 # pipes.sh -- animated pipes terminal screensaver
-brew install pipes-sh
+sudo brew install pipes-sh
 
 # thefuck -- corrects command mistakes
-brew install thefuck
+sudo brew install thefuck
 
 # gh -- github cli
-brew install gh
+sudo brew install gh
 
 # configure dotfiles
 cd ~
-rm *
+sudo rm *
 git clone https://github.com/joelflaig/dotfiles.git ~/dotfiles
 cd dotfiles
 stow .
